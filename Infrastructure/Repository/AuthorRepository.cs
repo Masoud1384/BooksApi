56e3ee86171c6a4807c696e53af59db61a681829
masoud <mwsoush.84@gmail.com>
2023-10-19 09:43:59 -0700

    Author repository has initialized

--- Infrastructure/Repository/AuthorRepository.cs
+++ Infrastructure/Repository/AuthorRepository.cs
@@ -1,39 +1,82 @@
 ﻿using Domain.IRepository;
 using Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace Infrastructure.Repository
 {
     public class AuthorRepository : IAuthorRepository
     {
+        private readonly Context _context;
+
+        public AuthorRepository(Context context)
+        {
+            _context = context;
+        }
+
         public bool Create(Author author)
         {
-            
+            try
+            {
+                _context.authors.Add(author);
+                var result = _context.SaveChanges();
+                return result == 1;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var author = _context.authors.Find(id);
+                var result = _context.SaveChanges();
+                return result == 1;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public Author Get(Expression<Func<Author, bool>> expression)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _context.authors.FirstOrDefault(expression);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         public List<Author> GetAuthors()
         {
-            throw new NotImplementedException();
+            return _context.authors.AsNoTracking().ToList();
         }
 
         public List<Author> GetAuthors(Expression<Func<Author, bool>> expression)
         {
-            throw new NotImplementedException();
+            var result = _context.authors.AsNoTracking().Where(expression);
+            return result.ToList();
         }
 
         public bool Update(Author author)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.authors.Update(author);
+                var result = _context.SaveChanges();
+                return result == 1;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
