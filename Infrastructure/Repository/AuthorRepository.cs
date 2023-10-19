eae4a63827f96483bb757ec6bbd00314e87a454e
masoud <mwsoush.84@gmail.com>
2023-10-19 09:21:16 -0700

    Infrastructure repositories implemented

---
+++ Infrastructure/Repository/AuthorRepository.cs
@@ -0,0 +1,39 @@
+﻿using Domain.IRepository;
+using Domain.Models;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repository
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        public bool Create(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Get(Expression<Func<Author, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Author> GetAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Author> GetAuthors(Expression<Func<Author, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Author author)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/Repository/BookRepository.cs
@@ -0,0 +1,39 @@
+﻿using Domain.IRepository;
+using Domain.Models;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        public bool Create(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book Get(Expression<Func<Book, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetBooks(Expression<Func<Book, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
