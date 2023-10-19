bd62e834b5725653aab4acaf49b19d674bdba66e
masoud <mwsoush.84@gmail.com>
2023-10-19 09:58:11 -0700

    Book commands are added to application layer

---
+++ Application/Commands/Books/CreateBookCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.Commands.Books
+{
+    public class CreateBookCommand
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime PublishDate { get; private set; }
+
+        public CreateBookCommand()
+        {
+            
+        }
+
+        public CreateBookCommand(string name, string description, DateTime publishDate)
+        {
+            Name = name;
+            Description = description;
+            PublishDate = publishDate;
+        }
+    }
+}
---
+++ Application/Commands/Books/UpdateBookCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Commands.Books
+{
+    public class UpdateBookCommand : CreateBookCommand
+    {
+        public int id { get; set; }
+        public UpdateBookCommand()
+        {
+
+        }
+        public UpdateBookCommand(int id, string name, string description, DateTime publishDate)
+            : base(name, description, publishDate)
+        {
+            this.id = id;
+        }
+    }
+}
