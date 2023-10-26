1348caac09d8fad3bf90e7486fc8d0398ddcdd2f
masoud <mwsoush.84@gmail.com>
2023-10-26 09:21:41 -0700

    Author and book application contracts added

---
+++ Application/Commands/Authors/IAuthorApplicationContract.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using System.Linq.Expressions;
+
+namespace Application.Commands.Authors
+{
+    public interface IAuthorApplicationContract
+    {
+        List<AuthorViewModel> SelectAllAuthors();
+        AuthorViewModel FindAuthorBy(Expression<Func<Author, bool>> expression);
+        bool DeActiveAuthor(int authorId);
+        bool AddUser(CreateAuthorCommand author, out int? authorId);
+        bool Update(UpdateAuthorCommand author);
+        bool ActivateAuthor(int authorId);
+    }
+}
---
+++ Application/Commands/Books/IBookApplicationContract.cs
@@ -0,0 +1,16 @@
+﻿using Application.Commands.Authors;
+using Domain.Models;
+using System.Linq.Expressions;
+
+namespace Application.Commands.Books
+{
+    public interface IBookApplicationContract
+    {
+        List<AuthorViewModel> SelectAllBooks();
+        AuthorViewModel FindBookBy(Expression<Func<Book, bool>> expression);
+        bool DeActiveBook(int bookId);
+        bool AddBook(CreateBookCommand book, out int? bookId);
+        bool Update(UpdateBookCommand book);
+        bool ActivateBook(int bookId);
+    }
+}
