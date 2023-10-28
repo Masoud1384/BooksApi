8fe083f38350f4e3b8756351e7ce436905b8776d
masoud <mwsoush.84@gmail.com>
2023-10-27 18:57:17 -0700

    Services are injected

---
+++ Infrastructure/Bootstraper.cs
@@ -0,0 +1,20 @@
+﻿using Application;
+using Domain.IRepository;
+using Microsoft.Extensions.DependencyInjection;
+using Application.Commands.Authors;
+using Application.Commands.Books;
+using Infrastructure.Repository;
+
+namespace Infrastructure
+{
+    public class Bootstraper
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            services.AddScoped<IAuthorApplicationContract,AuthorApplicationContracts>();
+            services.AddScoped<IBookApplicationContract, BoolApplicationContrracts>();
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+        }
+    }
+}
