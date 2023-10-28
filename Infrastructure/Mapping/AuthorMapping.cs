079768c9c6dc900281280d36e3c4ed6ce1b9b0ac
masoud <mwsoush.84@gmail.com>
2023-10-27 18:02:56 -0700

    Mapping has been added to the models

---
+++ Infrastructure/Mapping/AuthorMapping.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Reflection.Emit;
+
+namespace Infrastructure.Mapping
+{
+    public class AuthorMapping : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .UseIdentityColumn(1000, 1);
+            builder.Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(u => u.Active)
+                .IsRequired()
+                .HasConversion(new BoolToStringConverter("DeActive", "Active"));
+        }
+    }
+}
---
+++ Infrastructure/Mapping/BookMapping.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.Mapping
+{
+    public class BookMapping : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property<DateTime>("PublishDate")
+                .HasDefaultValue(DateTime.Now);
+
+            builder.Property(a => a.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(b=>b.Description)
+                .IsRequired()
+                .HasMaxLength(228);
+
+            builder.Property(t => t.Active)
+                .IsRequired()
+                .HasConversion(new BoolToStringConverter("DeActivated","Activated"));
+        }
+    }
+}
