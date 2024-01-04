53251d73ecdabf33b4fa7187eca0c9c545433aeb
masoud <mwsoush.84@gmail.com>
2024-01-04 12:59:01 -0800

    refresh token has added to database through the migration

---
+++ Infrastructure/Migrations/20240104205457_Refresh token being added.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Refreshtokenbeingadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "tokens",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExp",
+                table: "tokens",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "tokens");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExp",
+                table: "tokens");
+        }
+    }
+}
